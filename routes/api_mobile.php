6f0c24068cf0796c4fc5dc553ce4a6ce4ff423e0
drrowly99 <drrowly99@gmail.com>
2023-03-10 18:07:00 -0800

    merge with Amos

--- routes/api_mobile.php
+++ routes/api_mobile.php
@@ -15,10 +15,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/mono', function () {
-    return 'mono';
-});
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::put('/setpin', [AuthController::class, 'setpin']);
